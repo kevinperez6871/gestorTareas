719f171d86d675b50f996e6e4e5608b7e6af3e06
albern210 <67246373+albern210@users.noreply.github.com>
2024-11-19 17:44:53 -0500

    tercera subida

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,132 +1,14 @@
-import banner from '../assets/banner.jpg'
-import stepimg from '../assets/stepBystep.jpg'
+
+
+import Header from '../components/Header'
+import Servicios from '../components/Servicios'
 
 const HomePage = () => {
     return (
         <>
 
-            <section className="text-gray-600 body-font">
-                <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-                    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                            Tus Tareas en
-                            <br className="hidden lg:inline-block" />
-                            un Solo Lugar
-                        </h1>
-                        <p className="mb-8 leading-relaxed">
-                            Crear una cuenta es fácil y rápido. Solo necesitas unos minutos para tener tu propio espacio donde organizar tus tareas y actividades escolares
-                        </p>
-                        <div className="flex justify-center">
-                            <button className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-md">
-                                Registrate!
-                            </button>
-                            <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-md">
-                                Inicia Sesión!
-                            </button>
-                        </div>
-                    </div>
-                    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                        <img className="object-cover object-center rounded" alt="hero" src={banner} />
-                        {/*  <img
-                            className="rounded-xl shadow-lg w-80 h-80 object-cover"
-                            src={banner}
-                            alt="Banner"
-                        /> */}
-                    </div>
-                </div>
-            </section>
-            {/* seccion 2 */}
-            <section className="text-gray-600 body-font">
-                <div className="container px-7 py-24 mx-auto flex flex-wrap">
-                    <div className="flex flex-wrap w-full">
-                        <img className="lg:w-3/5 md:w-1/2 object-cover object-center rounded-lg md:mt-0 mt-12" src={stepimg} alt="step" />
-                        {/*  <img
-                            className="rounded-xl shadow-lg w-80 h-80 object-cover"
-                            src={banner}
-                            alt="Banner"
-                        /> */}
-                        <div className="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6">
-
-                            {/** STEP 1 */}
-                            <div className="flex relative pb-12">
-                                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
-                                </div>
-                                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-yellow-500 inline-flex items-center justify-center text-white relative z-10">
-                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                        <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-                                    </svg>
-                                </div>
-                                <div className="flex-grow pl-4">
-                                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 1</h2>
-                                    <p className="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
-                                </div>
-                            </div>
-                            {/** STEP 2 */}
-                            <div className="flex relative pb-12">
-                                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
-                                </div>
-                                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-blue-500 inline-flex items-center justify-center text-white relative z-10">
-                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                        <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                                    </svg>
-                                </div>
-                                <div className="flex-grow pl-4">
-                                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 2</h2>
-                                    <p className="leading-relaxed">Vice migas literally kitsch +1 pok pok. Truffaut hot chicken slow-carb health goth, vape typewriter.</p>
-                                </div>
-                            </div>
-                            {/** STEP 3 */}
-                            <div className="flex relative pb-12">
-                                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
-                                </div>
-                                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-blue-500 inline-flex items-center justify-center text-white relative z-10">
-                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                        <circle cx="12" cy="5" r="3"></circle>
-                                        <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
-                                    </svg>
-                                </div>
-                                <div className="flex-grow pl-4">
-                                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 3</h2>
-                                    <p className="leading-relaxed">Coloring book nar whal glossier master cleanse umami. Salvia +1 master cleanse blog taiyaki.</p>
-                                </div>
-                            </div>
-                            {/** STEP 4 */}
-                            <div className="flex relative pb-12">
-                                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
-                                </div>
-                                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-blue-500 inline-flex items-center justify-center text-white relative z-10">
-                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                                        <circle cx="12" cy="7" r="4"></circle>
-                                    </svg>
-                                </div>
-                                <div className="flex-grow pl-4">
-                                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 4</h2>
-                                    <p className="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
-                                </div>
-                            </div>
-                            {/** FINISH */}
-                            <div className="flex relative">
-                                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-blue-500 inline-flex items-center justify-center text-white relative z-10">
-                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                        <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                                        <path d="M22 4L12 14.01l-3-3"></path>
-                                    </svg>
-                                </div>
-                                <div className="flex-grow pl-4">
-                                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">FINISH</h2>
-                                    <p className="leading-relaxed">Pitchfork ugh tattooed scenester echo park gastropub whatever cold-pressed retro.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </section>
+            <Header />
+            <Servicios />
         </>
     )
 }
