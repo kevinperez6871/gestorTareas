20efc387c85618157f139548a649bf74d9f42d8b
kevin perez <159580550+kevinperez6871@users.noreply.github.com>
2024-11-25 23:34:57 -0500

    Api - Actualizacion grid

--- frontend/src/pages/ApiPage.jsx
+++ frontend/src/pages/ApiPage.jsx
@@ -22,25 +22,29 @@ export default function Api() {
     <>
       <div>
         <section>
-          <h1 className="text-8xl font-bold text-center my-10 text-black">
-            The Rick and Morty API
-          </h1>
+          <h1 className="text-8xl font-bold text-center my-10 text-black">The Rick and Morty API</h1>
         </section>
+        
         <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7 p-8">
           {data.map((character) => (
-            <div key={character.id} className="flex items-center bg-white rounded-lg shadow-md p-4" >
+           
+           <div key={character.id} className="flex items-center bg-white rounded-lg shadow-md p-4" >
               {/* Imagen del personaje */}
-              <img src={character.image} alt={character.name} className="w-24 h-24 rounded-md" />
+              <img className="w-41 h-30 rounded-md" src={character.image} alt={character.name}/>
               {/* Información del personaje */}
               <div className="ml-4 text-right flex-1">
-                <h2 className="font-bold text-xl text-white  hover:text-orange-600" >{character.name}</h2>
+                <h2 className="cursor-pointer font-bold text-xl text-black  hover:text-orange-600">
+                  {character.name}
+                </h2>
                 <p className="text-gray-600 ">
                   <span className="font-medium">Species:</span> {character.species}
                 </p>
               </div>
             </div>  
+
           ))}
         </main>
+     
       </div>
     </>
   );
